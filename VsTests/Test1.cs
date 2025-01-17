620d3078d69bce53409da246876c1761d4b15820
philimonovoleg <philimonovoleg@gmail.com>
2025-01-17 21:33:48 +0200

    fix test

--- VsTests/Test1.cs
+++ VsTests/Test1.cs
@@ -9,7 +9,7 @@ namespace VsTests
         [TestMethod]
         public void CreateEngine_Success()
         {
-            using var engine = new TestEngine("C:\\Users\\User\\source\\repos\\MagicTests\\MagicTests.CLI\\bin\\Debug\\net8.0\\MagicTests.Core.dll");
+            using var engine = new TestEngine("MagicTests.Core.dll");
 
             var field = engine.GetType().GetField("_assemblies", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 
@@ -17,7 +17,6 @@ namespace VsTests
             var collection = (ImmutableArray<string>)value;
 
             Assert.AreEqual(1, collection.Length);
-            Assert.AreEqual("C:\\Users\\User\\source\\repos\\MagicTests\\MagicTests.CLI\\bin\\Debug\\net8.0\\MagicTests.Core.dll", collection[0]);
         }
 
         [TestMethod]
