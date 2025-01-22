53a3f3a2da4612e5441692f92557e374664874a8
philimonovoleg <philimonovoleg@gmail.com>
2025-01-22 22:43:13 +0200

    add some additional things

--- MagicTests.Abstractions/Exceptions/AssertFailedException.cs
+++ MagicTests.Abstractions/Exceptions/AssertFailedException.cs
@@ -6,6 +6,9 @@
         public AssertFailedException(string? expected, string? actual)
             : base($"Actual: {actual}, expected: {expected}") { }
 
+        public AssertFailedException(string? expected, string? actual, Exception inner)
+            : base($"Actual: {actual}, expected: {expected}", inner) { }
+
         public AssertFailedException(string message) : base(message) { }
 
         public AssertFailedException(string message, Exception inner) : base(message, inner) { }
